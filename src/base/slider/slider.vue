517e4bb2c3c4d349a2d5adc8846bb15a332f7f00
zeeliu <zeeliu666@163.com>
2018-06-05 19:52:47 +0800

    20180605/singer

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -24,7 +24,23 @@ export default {
     },
     interval: {
       type: Number,
-      default: 3000
+      default: 1300
+    },
+    showDot: {
+      type: Boolean,
+      default: true
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    threshold: {
+      type: Number,
+      default: 0.3
+    },
+    speed: {
+      type: Number,
+      default: 400
     }
   },
   data: function () {
@@ -36,42 +52,74 @@ export default {
   },
   //  生命周期钩子；dom已经加载完执行这个钩子；如果是要操作dom一般是要在这个之后钩子或之后才行
   mounted() {
-    setTimeout(() => {
-      this._setSliderWidth()
-      this._initDots()
-      this._initSlider()
-      if (this.autoPlay) {
-        this._play()
-      }
-    }, 20)
+    this.update()
     window.addEventListener('resize', () => {
-      if (!this.slider) {
+      console.log('改变窗口')
+      //  enabled属性判断scroll是否处于启动状态
+      if (!this.slide | !this.slide.enabled) {
         return
       }
-      console.log('改变窗口')
-      this._setSliderWidth(true)
-      //  refresh是better-scroll的一个接口重新new BScroll
-      this.slider.refresh()
-      if (this.autoPlay) {
-        this._play()
-      }
+      console.log('改变窗口2')
+      clearTimeout(this.resizeTimer)
+      this.resizeTimer = setTimeout(() => {
+        this.refresh()
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 60)
     })
   },
   //  生命周期钩子；keep-alive 组件激活时调用。该钩子在服务器端渲染期间不被调用。
   activated() {
     if (this.autoPlay) {
-      this._play()
+      console.log('组件激活了')
+      this.refresh()
     }
   },
-  //  生命周期钩子；实例销毁之前调用。在这一步，实例仍然完全可用。该钩子在服务器端渲染期间不被调用
-  deactivated() {
-    clearTimeout(this.timer)
-  },
-  //  生命周期钩子；Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，所有的事件监听器会被移除，所有的子实例也会被销毁。
-  beforeDestroy() {
-    clearTimeout(this.timer)
-  },
+  // //  生命周期钩子；实例销毁之前调用。在这一步，实例仍然完全可用。该钩子在服务器端渲染期间不被调用
+  // deactivated() {
+  //   clearTimeout(this.timer)
+  // },
+  // //  生命周期钩子；Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，所有的事件监听器会被移除，所有的子实例也会被销毁。
+  // beforeDestroy() {
+  //   clearTimeout(this.timer)
+  // },
   methods: {
+    update() {
+      if (this.slide) {
+        // 销毁slide实例
+        this.slide.destroy()
+      }
+      // $nextTick在dom渲染好后执行
+      this.$nextTick(() => {
+        this.init()
+      })
+    },
+    // 重新计算scroll
+    refresh() {
+      this._setSliderWidth(true)
+      this.slide.refresh()
+    },
+    //  上一页
+    prev() {
+      this.slide.prev()
+    },
+    //  下一页
+    next() {
+      this.slide.next()
+    },
+    init() {
+      clearTimeout(this.timer)
+      this.pageIndex = 0
+      this._setSliderWidth()
+      if (this.showDot) {
+        this._initDots()
+      }
+      this._initSlider()
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
     _setSliderWidth(isResize) {
       this.children = this.$refs.group.children
       let width = 0
@@ -88,45 +136,45 @@ export default {
       this.$refs.group.style.width = width + 'px'
     },
     _initSlider() {
-      this.slider = new BScroll(this.$refs.slider, {
+      this.slide = new BScroll(this.$refs.slider, {
         scrollX: true,
         scrollY: false,
         momentum: false,
+        click: this.click,
+        bounce: false,
+        stopPropagation: true,
         snap: true,
         snapLoop: this.loop,
-        snapThreshold: 0.3,
-        snapSpeed: 400,
-        click: true
+        snapThreshold: this.threshold,
+        snapSpeed: this.speed
       })
-      this.slider.on('scrollEnd', () => {
-        let index = this.slider.getCurrentPage().pageX
-        if (this.loop) {
-          index -= 1
-        }
-        this.pageIndex = index
+      this.slide.on('scrollEnd', this._onScrollEnd)
+      this.slide.on('touchEnd', () => {
         if (this.autoPlay) {
-          clearTimeout(this.timer)
           this._play()
         }
       })
-      // this.slider.on('beforeScrollStart', () => {
-      //   if (this.autoPlay) {
-      //     clearTimeout(this.timer)
-      //   }
-      // })
+      this.slide.on('beforeScrollStart', () => {
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+        }
+      })
+    },
+    _onScrollEnd() {
+      let index = this.slide.getCurrentPage().pageX
+      this.pageIndex = index - 1
+      if (this.autoPlay) {
+        this._play()
+      }
     },
     _initDots() {
-      for (let i = 0; i < this.children.length; i++) {
-        this.dots.push('')
-      }
+      this.dots = new Array(this.children.length)
     },
     _play() {
-      let index = this.pageIndex + 1
-      if (this.loop) {
-        index += 1
-      }
+      // console.log(this.pageIndex)
+      clearTimeout(this.timer)
       this.timer = setTimeout(() => {
-        this.slider.goToPage(index, 0, 400)
+        this.next()
       }, this.interval)
     }
   }
